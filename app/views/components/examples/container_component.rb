e858ff09a68eab520f984496cc98b7befd12793c
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-09-06 16:03:00 +0100

    Add max width of 4xl to Examples::ContainerComponent

--- app/views/components/examples/container_component.rb
+++ app/views/components/examples/container_component.rb
@@ -1,7 +1,7 @@
 module Examples
   class ContainerComponent < ApplicationComponent
     def view_template(&)
-      div class: "space-y-8 pb-8 overflow-hidden", &
+      div class: "space-y-8 pb-8 overflow-hidden max-w-4xl", &
     end
   end
 end
