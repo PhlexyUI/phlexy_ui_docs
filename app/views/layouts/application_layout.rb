e92b57ff0cca5bd5d9a7d2720505509e553421e2
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-09-17 18:25:57 +0200

    Add Github link to the navbar

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -30,9 +30,26 @@ class ApplicationLayout < ApplicationView
           Drawer id: :drawer, lg: :open do |drawer|
             drawer.toggle
             drawer.content do
-              Navbar do
-                drawer.button :ghost, :square, class: "lg:hidden" do
-                  burger_svg
+              Navbar do |navbar|
+                navbar.start do
+                  drawer.button :ghost, :square, class: "lg:hidden" do
+                    burger_svg
+                  end
+                end
+
+                navbar.center do
+                end
+
+                navbar.end do
+                  Button(
+                    :ghost,
+                    :square,
+                    as: :a,
+                    href: "https://github.com/phlexyUI/phlexy_ui",
+                    target: "_blank"
+                  ) do
+                    github_svg
+                  end
                 end
               end
 
@@ -65,6 +82,20 @@ class ApplicationLayout < ApplicationView
         stroke_linejoin: "round",
         stroke_width: "2",
         d: "M4 6h16M4 12h16M4 18h16"
+      )
+    end
+  end
+
+  def github_svg
+    svg(
+      viewbox: "0 0 16 16",
+      class: "w-5 h-5",
+      fill: "currentColor",
+      aria_hidden: "true"
+    ) do |s|
+      s.path(
+        d:
+          "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
       )
     end
   end
