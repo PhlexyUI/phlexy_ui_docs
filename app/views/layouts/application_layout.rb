5164382d2678ab4fb5b2d1e90d5a4c77434d9943
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-09-18 02:02:11 +0200

    Add favicon and icons

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -20,6 +20,70 @@ class ApplicationLayout < ApplicationView
         plain helpers.vite_javascript_tag("application", data_turbo_track: "reload", type: "module")
 
         turbo_refreshes_with method: :morph, scroll: :preserve
+
+        link(
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png"
+        )
+
+        link(
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png"
+        )
+
+        link(
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-chrome-192x192.png"
+        )
+
+        link(
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png"
+        )
+
+        link(
+          rel: "manifest",
+          href: "/site.webmanifest"
+        )
+
+        link(
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#c94040"
+        )
+
+        meta(
+          name: "apple-mobile-web-app-title",
+          content: "PhlexyUI Docs"
+        )
+
+        meta(
+          name: "application-name",
+          content: "PhlexyUI Docs"
+        )
+
+        meta(
+          name: "msapplication-TileColor",
+          content: "#b91d47"
+        )
+
+        meta(
+          name: "msapplication-TileImage",
+          content: "/mstile-144x144.png"
+        )
+
+        meta(
+          name: "theme-color",
+          content: "#ffffff"
+        )
+
         yield :head
       end
 
